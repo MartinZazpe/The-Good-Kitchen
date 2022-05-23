b9ac84498359e03a25479ca684bee38653c9e5a2
MartinZazpe <martinzazpe1@gmail.com>
2022-05-23 11:17:35 +0200

    changes to the navbar

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -3,12 +3,37 @@ window.addEventListener("load", function () {
 
     var hamburguerMenu = document.querySelector(".hambMenu")
     var navBarList = document.querySelectorAll(".list")
-    var navbar = document.querySelector(".navbar")
+    var navbarId = document.querySelector("#navbar")
+    var flexboxInitial = document.querySelector(".flexboxInitial")
+    var flexboxExpanded = document.querySelector(".flexboxExpanded")
+    var navBarScreen = document.querySelector(".navBarScreen")
+    var menu = document.querySelector(".menu")
+    var logo = document.querySelector("#logo")
+    var loginRegister = document.querySelector(".login-register")
+    var noDisplay = document.querySelector(".noDisplay")
+
+    var order1 = document.querySelector(".order1")
+    var order2 = document.querySelector(".order2")
+    var order3 = document.querySelector(".order3")
 
     hamburguerMenu.addEventListener("click", function () {
         navBarList.forEach(function (list) {
             list.classList.toggle("listDisplay")
-            navbar.classList.toggle('navbarFixed')
+            // navbar.classList.toggle('navbarFixed')
+            navbarId.classList.toggle('navBarScreen')
+            if (navbarId.classList.contains('navBarScreen')) {
+                navbarId.classList.remove('flexboxInitial')
+                navbarId.classList.add('flexboxExpanded')
+                menu.classList.add('order2')
+                logo.classList.add('order1')
+                loginRegister.classList.add('noDisplay')
+            } else {
+                navbarId.classList.add('flexboxInitial')
+                navbarId.classList.remove('flexboxExpanded')
+                menu.classList.remove('order3')
+                logo.classList.remove('order1')
+                loginRegister.classList.remove('noDisplay')
+            }
         })
     })
 })
